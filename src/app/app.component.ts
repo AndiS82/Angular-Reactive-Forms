86b95f7a6b43faf4508b8e74fab069fc05f2901d
Andreas Schneider <as@MARS-SOLUTIONS.loc>
2023-05-08 08:45:45 +0200

    uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,55 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
 @ViewChild('f') signupForm: NgForm;
+defaultQuestion = 'pet';
+answer = '';
+genders=['male', 'female'];
+user={
+  username: '',
+  email:'',
+  secretQuestion:'',
+  answer:'',
+  gender:''
+};
+submitted=false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
-/*   onSubmit(form: NgForm){
-    console.log(form, ' submitted!');
-  } */
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
 
-  onSubmit(){
-    console.log(this.signupForm);
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.signupForm.reset();
+  }
+
+  resetAll(){
+    this.user.username = '';
+    this.user.email = '';
+    this.user.secretQuestion = 'pet';
+    this.user.answer = '';
+    this.user.gender = '';
   }
 }
